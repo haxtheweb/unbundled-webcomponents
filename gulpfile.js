b68e8bb5927ce67ea621384eb1baf68c7fc6f2de
btopro <btopro@outlook.com>
2020-03-02 21:09:31 -0500

    compression

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,8 @@
 const gulp = require('gulp');
 const terser = require('gulp-terser');
+const minifyJSTemplate = require('gulp-minify-html-literals');
 gulp.task(
   "default", async () => {
-    // not sure why but polymer isn't picked up unless we do this
-    await gulp.src('./build/es6/node_modules/@polymer/polymer/**/*.js')
-      .pipe(terser({
-        ecma: 2017,
-        keep_fnames: true,
-        mangle: false,
-        module: true,
-      }))
-      .pipe(gulp.dest('./build/es6/node_modules/@polymer/polymer/'));
     // now work on all the other files
     await gulp.src('./build/es6/**/*.js')
       .pipe(terser({
@@ -19,16 +11,8 @@ gulp.task(
         mangle: false,
         module: true,
       }))
+      .pipe(minifyJSTemplate())
       .pipe(gulp.dest('./build/es6/'));
-    // not sure why but polymer isn't picked up unless we do this
-    await gulp.src('./build/es6-amd/node_modules/@polymer/polymer/**/*.js')
-      .pipe(terser({
-        keep_fnames: true,
-        mangle: false,
-        module: true,
-        safari10: true,
-      }))
-      .pipe(gulp.dest('./build/es6-amd/node_modules/@polymer/polymer/'));
     // now work on all the other files
     return await gulp.src('./build/es6-amd/**/*.js')
       .pipe(terser({
@@ -37,6 +21,7 @@ gulp.task(
         module: true,
         safari10: true,
       }))
+      .pipe(minifyJSTemplate())
       .pipe(gulp.dest('./build/es6-amd/'));
   }
 );
