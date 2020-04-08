0142170d6b6c994a5e9e034b422f58cd055585d7
btopro <btopro@outlook.com>
2020-04-07 20:57:00 -0400

    wc-registry creation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
-const gulp = require('gulp');
-const terser = require('gulp-terser');
+const gulp = require("gulp");
+const terser = require("gulp-terser");
+const glob = require("glob");
+const fs = require("fs");
+const path = require("path");
 const minifyJSTemplate = require('gulp-minify-html-literals');
 gulp.task(
-  "default", async () => {
+  "terser", () => {
     // now work on all the other files
-    await gulp.src('./build/es6/**/*.js')
+    gulp.src('./build/es6/**/*.js')
       .pipe(terser({
         ecma: 2017,
         keep_fnames: true,
@@ -14,7 +17,7 @@ gulp.task(
       .pipe(minifyJSTemplate())
       .pipe(gulp.dest('./build/es6/'));
     // now work on all the other files
-    return await gulp.src('./build/es6-amd/**/*.js')
+    return gulp.src('./build/es6-amd/**/*.js')
       .pipe(terser({
         keep_fnames: true,
         mangle: false,
@@ -25,3 +28,72 @@ gulp.task(
       .pipe(gulp.dest('./build/es6-amd/'));
   }
 );
+
+gulp.task("wc-autoloader", async () => {
+  glob(path.join("./build/es6/node_modules/**/*.js"), (er, files) => {
+    let elements = {};
+    // async loop over files
+    files.forEach((file) => {
+      // grab the name of the file
+      if (fs.existsSync(file)) {
+        let fLocation = file.replace("build/es6/node_modules/", "");
+        const contents = fs.readFileSync(file, "utf8");
+        // This Regex is looking for tags that are defined by string values
+        // this will work for customElements.define("local-time",s))
+        // This will NOT work for customElements.define(LocalTime.tagName,s))
+        const defineStatements = /customElements\.define\(["|'|`](.*?)["|'|`]/gm.exec(
+          contents
+        );
+        // basic
+        if (defineStatements) {
+          elements[defineStatements[1]] = fLocation;
+        }
+        // .tag calls
+        else {
+          const hasDefine = /customElements\.define\((.*?),(.*?)\)/gm.exec(
+            contents
+          );
+          // check for a define still
+          if (hasDefine && hasDefine[1] && hasDefine[1].includes('.tag')) {
+            const tagStatements = /static get tag\(\){return"(.*?)"}/gm.exec(
+              contents
+            );
+            if (tagStatements) {
+              elements[tagStatements[1]] = fLocation;
+            }
+          }
+          else if (hasDefine && hasDefine[1] && hasDefine[1].includes('.is')) {
+            const tagStatements = /static get is\(\){return"(.*?)"}/gm.exec(
+              contents
+            );
+            if (tagStatements) {
+              elements[tagStatements[1]] = fLocation;
+            }
+          }
+          else {
+            if (!hasDefine) {
+              // support for polymer legacy class housing
+              const PolymerLegacy = /\,is\:\"(.*?)\",/gm.exec(
+                contents
+              );
+              if (PolymerLegacy && PolymerLegacy[1]) {
+                elements[PolymerLegacy[1]] = fLocation;
+              }
+              else {
+                // if we got here, it wasn't a file w/ a custom element definition
+                // so it's not an entry point
+              }
+            }
+          }
+        }
+      }
+    });
+
+    // write entries to file
+    fs.writeFileSync(
+      path.join(__dirname, "wc-registry.json"),
+      JSON.stringify(elements),
+      "utf8"
+    );
+  });
+});
