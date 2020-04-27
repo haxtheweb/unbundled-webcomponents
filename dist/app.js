915dea8c3f8dbc76bae5dd2f5ff7a69a7bef08b7
Bryan Ollendyke <btopro@outlook.com>
2020-04-27 15:06:58 -0400

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,6 @@
 // import "@lrnwebcomponents/h-a-x/h-a-x.js";
 // import "@lrnwebcomponents/hax-body/hax-body.js";
 // import "@lrnwebcomponents/hax-body-behaviors/hax-body-behaviors.js";
-// import "@lrnwebcomponents/hax-body-behaviors/hax-body-behaviors.js";
 // import "@lrnwebcomponents/product-card/lib/hax-element-list-selector.js";
 // import "@lrnwebcomponents/cms-hax/cms-hax.js";
 // import "@lrnwebcomponents/wysiwyg-hax/wysiwyg-hax.js";
