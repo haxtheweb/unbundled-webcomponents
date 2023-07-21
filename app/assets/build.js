85f4184a6c8a52feea1c3c2f8b62da05a3b0a175
btopro <btopro@outlook.com>
2023-07-21 10:22:49 -0400

    https://github.com/elmsln/issues/issues/1617

--- app/assets/build.js
+++ app/assets/build.js
@@ -21,10 +21,12 @@ if (window.WCGlobalRegistryFileName) {
 }
 // the full patht o accomplish autoloading
 window.WCAutoloadRegistryFile = cdn + fname;
-// this try block ensures that if anything fails here we always fall back
-// to the ES5 version of the assets. While imperfect, a dynamic import()
-// will fall almost perfectly along the lines of what to ship the
-// end user
+// location of the autoloader which we use for the entryway to the universe
+// this is what does all the work in the magic script (tm)
+var autoloaderScriptLocation = "build/es6/node_modules/@lrnwebcomponents/wc-autoload/wc-autoload.js";
+// this try block ensures that if anything fails here we don't support it
+// with a modern interface and it'll be a iframe structure. While imperfect, a dynamic import()
+// will fall almost perfectly along the lines of what to ship the end user
 try {
   // find the 1st script tag. We know this exists because our own script HAD
   // to be appended to the document that is running. Most likely this finds
@@ -34,19 +36,13 @@ try {
   // this has to be run as a Function executed on its own otherwise FF and
   // older platforms will bomb because they read this as a parse error
   new Function("import('');");
-  // insert polyfill for web animations. We don't get here in legacy platforms
-  // and this is because not everything supports web animations and it's a popular
-  // thing to implement in advanced web development
-  if (!Element.prototype.animate){
-    var ani = document.createElement("script");
-    ani.src = cdn + "build/es6/node_modules/web-animations-js/web-animations-next-lite.min.js";
-    def.parentNode.insertBefore(ani, def);
-  }
+  animationPolyfillTest();
   // create our autoloader script, which is a JS module and inject into the dom
   // this does all the real work of automatically loading our web components
   var build = document.createElement("script");
-  build.src = cdn + "build/es6/node_modules/@lrnwebcomponents/wc-autoload/wc-autoload.js";
+  build.src = cdn + autoloaderScriptLocation;
   build.type = "module";
+  build.onerror = (e) => { fallbackChecks(e);};
   def.parentNode.insertBefore(build, def);
 } catch (err) {
   // legacy platforms skip to this step. At this point we know we can inject
@@ -66,4 +62,54 @@ try {
   var buildLegacy = document.createElement("script");
   buildLegacy.src = cdn + "assets/build-legacy.js";
   def.parentNode.insertBefore(buildLegacy, def);
+}
+// animation polyfill since some platforms are modern but don't entirely support this
+function animationPolyfillTest() {
+    // insert polyfill for web animations. We don't get here in legacy platforms
+  // and this is because not everything supports web animations and it's a popular
+  // thing to implement in advanced web development
+  if (!Element.prototype.animate){
+    var ani = document.createElement("script");
+    ani.src = window.__appCDN + "build/es6/node_modules/web-animations-js/web-animations-next-lite.min.js";
+    def.parentNode.insertBefore(ani, def);
+  }
+}
+// if we fail to load the module, we need to fall back to known sources
+// and reattempt to inject the CDN location. This is a last ditch effort
+// though opens the ability for us to download user sites and if plugged
+// into other hax housing then it'll work but also just work stand alone
+// as a downloaded package
+function fallbackChecks(e) {
+    var cdn = window.__appCDN;
+    if (!window.__appCDNBlockFallback) {
+        var build = document.createElement("script");
+        build.type = "module";
+        build.onerror = (e) => { fallbackChecks(e);};
+        // if the module fails to load at the set CDN location, try to fail back to known sources
+        if (cdn === "./") {
+            // psu fallback
+            window.__appCDN = "https://cdn.webcomponents.psu.edu/cdn/";
+            window.WCAutoloadRegistryFile = window.WCAutoloadRegistryFile.replace(cdn, window.__appCDN);
+            build.src = window.__appCDN + autoloaderScriptLocation;
+            def.parentNode.insertBefore(build, def);
+            console.warn(cdn + " failed to respond, falling back to alternative: " + window.__appCDN);
+        }
+        else if (cdn === "https://cdn.webcomponents.psu.edu/cdn/") {
+            // known mirror
+            window.__appCDN = "https://cdn.waxam.io/";
+            window.WCAutoloadRegistryFile = window.WCAutoloadRegistryFile.replace(cdn, window.__appCDN);
+            build.src = window.__appCDN + autoloaderScriptLocation;
+            def.parentNode.insertBefore(build, def);
+            console.warn(cdn + " failed to respond, falling back to alternative: " + window.__appCDN);
+        }
+        else {
+            // :( we're out of options, just reset these values to default
+            // but we aren't working in this scenario bc both CDNs failed
+            // and local delivery could not be found
+            window.__appCDN = "./";
+            window.WCAutoloadRegistryFile = window.WCAutoloadRegistryFile.replace(cdn, window.__appCDN);
+            console.error("Local delivery failed and all alternative CDNs failed to load. You might be offline, in a secure environment or doing testing intentionally to generate this *shrug*");
+        }
+        animationPolyfillTest(); // if we just failed previously, we need to retest bc that would fail too
+    }
 }
